60b4c3df1105779704bbf0bd6c5e8187cf5b36af
Gawali Krushna <gkrushna893@gmail.com>
2026-06-22 21:23:20 +0530

    Add files via upload

---
+++ Python Assignment Codes/DataType.py
@@ -0,0 +1,5 @@
+a = int(input("Enter String or number: "))
+
+print("Data type: ",type(a))
+print("Memory Address: ",id(a))
+print("Size: ",a.__sizeof__())
---
+++ Python Assignment Codes/Function.py
@@ -0,0 +1,6 @@
+def Multiplication(a, b):
+    return a * b
+    
+print(Multiplication(5,4))
+
+
